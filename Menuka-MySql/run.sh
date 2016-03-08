6efe48a0e43119b8c13ff3e2d736bc85e05e1171
Menuka5 <menukaishan5@gmail.com>
2016-03-08 12:16:56 +0530

    removing unwanted files

--- Menuka-MySql/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn clean install;
-cp /home/hsenid/Software/projects/Menuka-MySql/target/Menuka-MySql.war /home/hsenid/Software/apache-tomcat-8.0.32/webapps
--- Menuka-MySql/run.sh~
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mvn clean install;
-cp /home/hsenid/Software/projects/Menuka-login/target/Menuka-login.war /home/hsenid/Software/apache-tomcat-8.0.32/webapps
