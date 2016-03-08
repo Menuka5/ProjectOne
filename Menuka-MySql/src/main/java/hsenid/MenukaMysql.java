b8f9354dae03916995f2b86c9784f1d359c272c1
Menuka5 <menukaishan5@gmail.com>
2016-03-08 13:52:31 +0530

    Comments added

--- Menuka-MySql/src/main/java/hsenid/MenukaMysql.java
+++ Menuka-MySql/src/main/java/hsenid/MenukaMysql.java
@@ -12,21 +12,29 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+
 public class MenukaMysql extends HttpServlet {
+
+//This class creates the connection between database and program. 
+//Then it will check the credintials are matching or not
 
     @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter(); // To print on HTML page
 
-        String name = request.getParameter("username");
+        String name = request.getParameter("username"); //Get the username and password parameters from jsp.
         String pass = request.getParameter("password");
 
-        boolean status = false;
+        boolean status = false; //To store the credintials matching
+
+// Database connection creation 
         Connection conn = null;
         PreparedStatement pst = null;
         ResultSet rs = null;
+
+//Database credintials
 
         String url = "jdbc:mysql://localhost:3306/";
         String dbName = "hsenid";
@@ -36,16 +44,19 @@ public class MenukaMysql extends HttpServlet {
 
         try {
             Class.forName(driver).newInstance();
-            conn = DriverManager.getConnection(url + dbName, userName, password);
+            conn = DriverManager.getConnection(url + dbName, userName, password); // Connecting the database
             pst = conn.prepareStatement("select * from users where username=? and password=?");
             pst.setString(1, name);
             pst.setString(2, pass);
 
             rs = pst.executeQuery();
-            status = rs.next();
+            status = rs.next(); // Status of matching password 
+
+//Print out the HTML content according to the matching credintials.
 
             if (status){
                 out.println("Success!");
+
             }else {
                 out.println("Failed!!");
             }
--- Menuka-login/src/main/java/hsenid/login/Login.java
+++ Menuka-login/src/main/java/hsenid/login/Login.java
@@ -7,17 +7,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//This class check the given credintials are matching with the embedded details.
+
 public class Login extends HttpServlet {
+
     @Override
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws IOException, ServletException{
         response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
+        PrintWriter out = response.getWriter(); // To write HTML contents
 
-        String username = request.getParameter("username");
+        String username = request.getParameter("username"); //Gets the username and password from jsp post method to these variables
         String password = request.getParameter("password");
 
-        if (username.equals("Admin") && password.equals("Admin123")){
+        if (username.equals("Admin") && password.equals("Admin123")){ //Checking whether password is matching or not.
             out.println("Successful!");
         }else{
             out.println("Failed!!!");
